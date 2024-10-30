2fba594c87515ebbab6ebd8d17271e3c5676f068
Sarun Maharjan <sarunmrzn@gmail.com>
2024-10-30 14:25:29 +0545

    add typescript format

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -35,6 +35,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
