d9cc181968296ff0b475a056e4aadf34d9cdf506
Sarun Maharjan <sarunmrzn@gmail.com>
2024-11-24 19:45:34 +0545

    exclude

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -4,30 +4,5 @@ require('lazy').setup({
   },
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'christoomey/vim-tmux-navigator',
-
-  -- require 'plugins/gitsigns',
-  --
-  -- require 'plugins/telescope',
-  --
-  -- require 'plugins/lspconfig',
-  --
-  -- require 'plugins/conform',
-  --
-  -- require 'plugins/cmp',
-  --
-  -- require 'plugins/tokyonight',
-  --
-  -- require 'plugins/todo-comments',
-  --
-  -- require 'plugins/mini',
-  --
-  -- require 'plugins/treesitter',
-  --
-  -- -- require 'plugins.debug',
-  -- require 'plugins.indent_line',
-  -- require 'plugins.lint',
-  -- require 'plugins.autopairs',
-  -- require 'plugins.neo-tree',
-
   checker = { enabled = true },
 }, {})
