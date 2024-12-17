1e85f63c9e148beb670f1248bfa4f3e68df15c49
Sarun Maharjan <sarunmrzn@gmail.com>
2024-12-17 11:22:54 +0545

    stuff

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -5,7 +5,7 @@ return {
     cmd = { 'ConformInfo' },
     keys = {
       {
-        '<leader>f',
+        '<leader>fx',
         function()
           require('conform').format { async = true, lsp_format = 'fallback' }
         end,
