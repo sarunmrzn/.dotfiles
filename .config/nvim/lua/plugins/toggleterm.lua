b4bdf2f84e2e2b970d3f536fded7a53f8f608e64
Sarun <sarunmrzn@gmail.com>
2025-02-10 12:43:54 +0545

    added lazydocker

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -55,9 +55,69 @@ return {
 				end,
 			})
 
+			local lazydocker = Terminal:new({
+				cmd = "lazydocker",
+				direction = "float",
+				size = 20,
+				open_mapping = [[<c-\>]],
+				hide_numbers = true,
+				shade_filetypes = {},
+				shade_terminals = false,
+				shading_factor = 2,
+				start_in_insert = true,
+				insert_mappings = true,
+				persist_size = true,
+				close_on_exit = true,
+				shell = vim.o.shell,
+				highlights = {
+					Normal = {
+						guibg = "NONE",
+					},
+					NormalFloat = {
+						guibg = "NONE",
+					},
+					FloatBorder = {
+						guifg = "NONE",
+						guibg = "NONE",
+					},
+				},
+				float_opts = {
+					border = "curved",
+					winblend = 0,
+					highlights = {
+						border = "Normal",
+						background = "None",
+					},
+				},
+				on_open = function(term)
+					vim.cmd("startinsert!")
+					vim.api.nvim_buf_set_keymap(
+						term.bufnr,
+						"n",
+						"q",
+						"<cmd>close<CR>",
+						{ noremap = true, silent = true }
+					)
+				end,
+				on_close = function()
+					vim.cmd("startinsert!")
+				end,
+			})
+
 			function _LAZYGIT_TOGGLE()
 				lazygit:toggle()
 			end
+
+			function _LAZYDOCKER_TOGGLE()
+				lazydocker:toggle()
+			end
+
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>D",
+				"<cmd>lua _LAZYDOCKER_TOGGLE()<CR>",
+				{ noremap = true, silent = true }
+			)
 
 			vim.api.nvim_set_keymap(
 				"n",
