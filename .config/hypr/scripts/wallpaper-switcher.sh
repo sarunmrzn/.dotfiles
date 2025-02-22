bdd20da78b15deaef07eedbbd42549ccc5806be6
Sarun <sarunmrzn@gmail.com>
2025-02-22 21:40:28 +0545

    stuff

--- .config/hypr/scripts/wallpaper-switcher.sh
+++ .config/hypr/scripts/wallpaper-switcher.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+hyprctl dispatch exec hyprpaper
 
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
 
-INTERVAL=900
+INTERVAL=300
 
 while true; do
+    sleep 1
     WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" \) | shuf -n 1)
     hyprctl hyprpaper reload ,"$WALLPAPER"
     wal -i "$WALLPAPER"
