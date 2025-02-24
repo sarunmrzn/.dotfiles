0402cdf4ee7363bcd20bb01a0658f1b5c5039e12
Sarun <sarunmrzn@gmail.com>
2025-02-24 17:34:23 +0545

    kk

--- .config/hypr/scripts/wallpaper-switcher.sh
+++ .config/hypr/scripts/wallpaper-switcher.sh
@@ -2,6 +2,7 @@
 
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
 
+sleep 0.5
 while true; do
     CURRENT_WALL=$(hyprctl hyprpaper listloaded)
     WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
